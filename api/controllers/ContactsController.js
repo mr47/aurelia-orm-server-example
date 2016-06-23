5063d2be28c7d30c94d26b0703f79ed2fc987fa4
dmitry.poddubniy <dmitry.poddubniy@antarasoft.com>
2016-06-23 19:19:50 +0300

    Initial commit

---
+++ api/controllers/ContactsController.js
@@ -0,0 +1,11 @@
+/**
+ * ContactsController
+ *
+ * @description :: Server-side logic for managing contacts
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Contacts.js
@@ -0,0 +1,30 @@
+/**
+ * Contacts.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  
+  attributes: {
+    id: {
+      type: "integer",
+      primaryKey: true
+    },
+    firstname: {
+      type: "string",
+      size: 100
+    },
+    lastname: {
+      type: "string",
+      size: 100
+    },
+    age: {
+      type: "integer",
+      size: 100,
+      defaultTo: null
+    }
+  }
+};
+
