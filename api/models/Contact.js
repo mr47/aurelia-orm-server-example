903c00d1d74c29c95abd81c75b92f1306804622a
dmitry.poddubniy <dmitry.poddubniy@antarasoft.com>
2016-06-23 19:39:00 +0300

    update server configuration

--- api/models/Contact.js
+++ api/models/Contact.js
@@ -1,12 +1,12 @@
 /**
- * Contacts.js
+ * Contact.js
  *
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
 module.exports = {
-  
+
   attributes: {
     id: {
       type: "integer",
